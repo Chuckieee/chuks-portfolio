02d1e730b820d049c662498bef784a9656d11132
chukwudi <chukwudinwanzuluahu@gmail.com>
2023-09-21 17:23:28 -0700

    hi world

---
+++ script/script.js
@@ -0,0 +1,38 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+function showPopupForm() {
+    document.getElementById("popup-form-container").style.display = "block";
+  }
+  
+  function hidePopupForm() {
+    document.getElementById("popup-form-container").style.display = "none";
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('show-popup').addEventListener('click', () => {
+      showPopupForm();
+    });
+  
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') hidePopupForm()
+    });
+  
+  });
+  document.getElementById("contact-form").addEventListener("submit", (event) => {
+    const contactForm = event.target
+    if (!validateContactForm(contactForm)) {
+      event.preventDefault();
+      displayError(contactForm, 'Invalid input')
+    }
+  });
+  
